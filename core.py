845e64cadc572395a518b2d7d1b5b55708c8b055
Nathan Slawson <urikkiru@gmail.com>
2019-07-22 19:14:09 -0700

    Initial commit, base functionality implemented. Still needs backup handling, and upgrade functionality.

---
+++ core.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python3
+import glob
+import os
+import shutil
+
+import docker
+import fire
+from jinja2 import Template
+from mcrcon import MCRcon
+import yaml
+
+from core_utils import downloadFile, setupLogging, unzipFile, resetPermissions, updateConfig, createEULA, getServerProperies
+
+
+log = setupLogging()
+
+
+class Config:
+    def __init__(self):
+        self.configFolder = os.path.join(os.getcwd(), "config")
+        self.instanceFolder = os.path.join(os.getcwd(), "instances")
+        self.workingDir = os.getcwd()
+        self.imageName = 'sirius2/server'
+        self.imageVersion = '1.0'
+        self.mountpoint = '/minecraft'
+
+
+class Core:
+    def __init__(self):
+        self.data = {}
+
+        self.config = Config()
+        # self.myDocker = DockerUtils()
+        self.client = docker.from_env()
+
+        self.__loadDefinitions()
+
+        if not os.path.exists(self.config.instanceFolder):
+            os.mkdir(self.config.instanceFolder)
+
+    def __loadDefinitions(self):
+        configList = glob.glob(os.path.join(self.config.configFolder, "*.yaml"))
+
+        for cRef in configList:
+            with open(cRef, "r", encoding='utf-8') as confFile:
+                cData = yaml.load(confFile, Loader=yaml.FullLoader)
+                self.data[os.path.splitext(os.path.basename(cRef))[0]] = cData
+
+    def help(self):
+        print('Help placeholder')
+
+    def build(self, nocache=False):
+        log.info('Building docker image')
+
+        targetFolder = self.config.workingDir
+        templateFilename = os.path.join(targetFolder, 'Dockerfile.template')
+        with open(templateFilename, 'r', encoding='utf-8') as tFile:
+            tData = tFile.read()
+            myTemplate = Template(tData)
+            outputData = myTemplate.render(
+                {
+                    'gid': os.getgid(),
+                    'uid': os.getuid()
+                }
+            )
+            with open(os.path.join(targetFolder, 'Dockerfile'), 'w', encoding='utf-8') as outFile:
+                outFile.write(outputData)
+
+        self.client.images.build(path=targetFolder, nocache=nocache, tag='{}:{}'.format(
+            self.config.imageName,
+            self.config.imageVersion
+        ))
+        self.client.images.build(path=targetFolder, nocache=nocache, tag='{}:latest'.format(self.config.imageName))
+
+    def download(self, name):
+        url = self.data[name]['url']
+        instanceFolder = os.path.join(self.config.instanceFolder, name)
+
+        if os.path.exists(instanceFolder):
+            raise FileExistsError('Instance folder "{}" already exists'.format(instanceFolder))
+
+        os.mkdir(instanceFolder)
+
+        log.info('[{}] Downloading {}'.format(name, url))
+        myFilename = downloadFile(url, instanceFolder)
+
+        log.info('Download Complete, extracting: {}'.format(myFilename))
+        unzipFile(myFilename, instanceFolder)
+
+        log.info('Resetting file permissions')
+        resetPermissions(instanceFolder)
+
+        os.chmod(os.path.join(instanceFolder, self.data[name]['entrypoint']), 0o0755)
+
+    def configure(self, name):
+        instanceFolder = os.path.join(self.config.instanceFolder, name)
+        for filename, data in self.data[name]['configs'].items():
+            log.info('Patching {}'.format(filename))
+
+            fullFilename = os.path.join(instanceFolder, filename)
+            updateConfig(fullFilename, data)
+
+        log.info('Creating eula.txt')
+        createEULA(instanceFolder)
+
+    def upgrade(self, name):
+        pass
+
+    def start(self, name):
+        log.info('Starting {}'.format(name))
+
+        instanceFolder = os.path.join(self.config.instanceFolder, name)
+        startCommand = os.path.join(self.config.mountpoint, self.data[name]['entrypoint'])
+        port = self.data[name]['port']
+        rconport = self.data[name].get('rconport')
+        print(startCommand)
+        self.client.containers.prune()
+
+        portmap = {
+            '{}/tcp'.format(port): '{}'.format(port)
+        }
+        if rconport:
+            portmap['{}/tcp'.format(rconport)] = rconport
+
+        self.client.containers.run(
+            image='{}:{}'.format(self.config.imageName, self.config.imageVersion),
+            command=startCommand,
+            name=name,
+            volumes={
+                instanceFolder: {
+                    'bind': self.config.mountpoint,
+                    'mode': 'rw'
+                }
+            },
+            ports=portmap,
+            user='minecraft',
+            working_dir=self.config.mountpoint,
+            detach=True,
+            stdin_open=True,
+            tty=True
+        )
+
+    def stop(self, name):
+        pass
+
+    def destroy(self, name, confirm=False):
+        if not name in self.data:
+            raise ValueError('{} not a valid instance name'.format(name))
+        myInstanceFolder = os.path.join(self.config.instanceFolder, name)
+        log.info('Destroying instance {} {}'.format(name, myInstanceFolder))
+
+        if not confirm:
+            log.warning('Dry run. Use "destroy --confirm=True" if you\'re really sure. All instance data will be deleted.')
+            return
+
+        # TODO stop any existing docker instance first
+
+        # This is required because sometimes mod pack config files have incorrect *nix permissions. This leads to weird
+        # scenarios where you don't have list access to the file/folder and therefore cannot delete it.
+        resetPermissions(myInstanceFolder)
+
+        if os.path.exists(myInstanceFolder):
+            shutil.rmtree(myInstanceFolder)
+
+    def install(self, name):
+        self.build()
+        self.download(name)
+        self.configure(name)
+        #self.start(name)
+
+    def exec(self, name, commStr):
+        if not commStr.startswith('/'):
+            commStr = '/' + commStr
+        instanceFolder = os.path.join(self.config.instanceFolder, name)
+        myProps = getServerProperies(instanceFolder)
+
+        if myProps.get('enable-rcon') == 'true':
+            with MCRcon(host='localhost', password=myProps.get('rcon.password'), port=int(myProps.get('rcon.port'))) as mcr:
+                resp = mcr.command(commStr)
+                log.info(resp)
+        else:
+            log.error('rcon disabled for this server instance')
+
+
+
+if __name__ == '__main__':
+    myCore = Core()
+    fire.Fire(myCore)
---
+++ core_utils.py
@@ -0,0 +1,116 @@
+import logging
+import os
+import re
+import subprocess
+
+import coloredlogs
+import requests
+
+def setupLogging(logname=__name__, level=logging.DEBUG, logFormat='%(asctime)s [%(levelname)s] %(message)s'):
+    myLog = logging.getLogger(logname)
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setLevel(level)
+    myFormat = logging.Formatter(logFormat)
+    consoleHandler.setFormatter(myFormat)
+    myLog.addHandler(consoleHandler)
+
+    myLog.setLevel(level)
+
+    coloredlogs.install(level=logging.getLevelName(level), logger=myLog, fmt=logFormat)
+
+    return myLog
+
+def downloadFile(url, folder=None, filename=None):
+    with requests.get(url, allow_redirects=True, stream=True) as myReq:
+        myReq.raise_for_status()
+        if not filename:
+            contentDisposition = myReq.headers.get('Content-Disposition')
+            if not contentDisposition:
+                filename = url.rsplit('/', 1)[1]
+            else:
+                filename = re.findall('filename=(.+)', contentDisposition)
+        if folder:
+            if not os.path.exists(folder):
+                os.mkdir(folder)
+            filename = os.path.join(folder, filename)
+
+        with open(filename, 'wb', ) as outFile:
+            for chunk in myReq.iter_content(chunk_size=8192):
+                if chunk: # filter out keep-alive new chunk
+                    outFile.write(chunk)
+
+    return filename
+
+def resetPermissions(folderName):
+    for root, dirs, files in os.walk(folderName):
+        for dRef in dirs:
+            os.chmod(os.path.join(root, dRef), 0o0755)
+        for fRef in files:
+            os.chmod(os.path.join(root, fRef), 0o0644)
+
+def unzipFile(filename, targetFolder):
+   command = ['unzip', filename, '-d', targetFolder]
+   myProc = subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+   if myProc.returncode >= 2:
+       raise RuntimeError('Unzip of {} -> {} failed.'.format(filename, targetFolder))
+
+def updateConfig(filename, data):
+    with open(filename, 'r', encoding='utf-8') as confFile:
+        confLines = confFile.readlines()
+
+    # Used to keep track of fields that already exist
+    keyMap = {}
+
+    for iRef in enumerate(confLines):
+        if '=' in iRef[1]:
+            key, value = re.findall('(.+)=(.+)', iRef[1], re.DOTALL)[0]
+            if key in data:
+                confLines[iRef[0]] = "{}={}\n".format(key, data.get(key))
+                keyMap[key] = data.get(key)
+            else:
+                keyMap[key] = value
+
+
+    with open(filename, 'w', encoding='utf-8') as confFile:
+        confFile.writelines(confLines)
+
+        # adding any fields that didn't exist previously
+        for key, value in data.items():
+            if not key in keyMap:
+                confFile.write("{}={}\n".format(key, value))
+
+
+def createEULA(folder):
+    myFilename = os.path.join(folder, 'eula.txt')
+
+    eulaStr = '''#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).
+#Mon Sep 29 23:27:36 UTC 2014
+eula=true'''
+
+    with open(myFilename, 'w', encoding='utf-8') as eulaFile:
+        eulaFile.write(eulaStr)
+
+def getServerProperies(targetFolder):
+    pFilename = os.path.join(targetFolder, 'server.properties')
+    retData = {}
+    with open(pFilename, 'r', encoding='utf-8') as pFile:
+        for lRef in pFile.readlines():
+            myLine = lRef.rstrip('\n')
+            pos = myLine.find('=')
+            if pos >= 0:
+                key = myLine[:pos]
+                value = myLine[pos+1:]
+                retData[key] = value
+
+    return retData
+
+
+if __name__ == '__main__':
+    #myFilename = downloadFile('https://media.forgecdn.net/files/2744/435/Enigmatica2Server-1.69a.zip')
+    #print(myFilename)
+
+    # log = setupLogging()
+    # log.info('Hi there')
+    myData = getServerProperies('instances/enigmatica2')
+    for dkey, dvalue in myData.items():
+        print( dkey, '=', dvalue )
